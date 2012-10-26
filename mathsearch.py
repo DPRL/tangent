ec24a5fb63094081c79dc3b8060a1f34a115edb1
David Stalnaker <david.stalnaker@gmail.com>
2012-10-25 22:05:42 -0400

    Implemented search, and added a home page.

--- mathsearch.py
+++ mathsearch.py
@@ -1,14 +1,25 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 from invertedfile import Symbol, SymbolTree, TreeIndex
 app = Flask(__name__)
 index = TreeIndex()
 
 @app.route('/')
-def hello():
-    results = sorted(index.search(a), reverse=True, key=lambda x: x[1])
+def root():
+    if 'query' in request.args:
+        return query()
+    else:
+        return home()
+
+def home():
+    return render_template('query.html')
+
+def query():
+    query = request.args['query']
+    t = SymbolTree.parse_from_tex(query)
+    results = sorted(index.search(t), reverse=True, key=lambda x: x[1])
     results = map(lambda x: ('$' + index.trees[x[0]].tex + '$', x[1]), results)
     print(results)
-    return render_template('results.html', results=results)
+    return render_template('results.html', query=query, results=results, num_results=len(results))
 
 if __name__ == '__main__':
 
--- static/main.js
+++ static/main.js
@@ -1,3 +1,17 @@
 MathJax.Hub.Config({
   tex2jax: {inlineMath: [['$','$'], ['\\(','\\)']]}
 });
+
+function submitQuery() {
+    var query = document.getElementById('query').value;
+    window.location = '/?query=' + encodeURIComponent(query);
+    console.log(query);
+}
+
+$(function() {
+    $('#query').keypress(function (e) {
+      if (e.which == 13) {
+        submitQuery();
+      }
+    });
+});
