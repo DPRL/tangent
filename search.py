6078eab29c47a8959b1a5cfbed10a61ba37b4421
David Stalnaker <david.stalnaker@gmail.com>
2013-04-25 17:50:08 -0400

    Added simple binary for searching.

---
+++ search.py
@@ -0,0 +1,27 @@
+from sys import argv, exit
+
+from werkzeug.utils import import_string
+
+from tangent import RedisIndex, SymbolTree
+
+def search(config, query):
+    index = RedisIndex(db=config.DATABASE, ranker=config.RANKER)
+    results = index.search_tex(query)
+    first = results.next()
+    print(first[0])
+        
+
+def print_help_and_exit():
+    exit('Usage: python search.py config_object query')
+
+if __name__ == '__main__':
+
+    if len(argv) > 2:
+        if argv[1] == 'help':
+            print_help_and_exit()
+        else:
+            config = import_string(argv[1])
+            query = argv[2]
+            search(config, query)
+    else:
+        print_help_and_exit()
