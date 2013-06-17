d869a3e302f10c3850044fcc1b296e03c2057c53
David Stalnaker <david.stalnaker@gmail.com>
2013-06-17 16:12:33 -0400

    Search.py now outputs JSON

--- search.py
+++ search.py
@@ -1,4 +1,5 @@
-from sys import argv, exit
+from sys import argv, exit, stdin
+import json
 
 from werkzeug.utils import import_string
 
@@ -7,21 +8,28 @@ from tangent import RedisIndex, SymbolTree
 def search(config, query):
     index = RedisIndex(db=config.DATABASE, ranker=config.RANKER)
     results, _, _ = index.search_tex(query)
-    first = results[0]
-    print(first[0])
+    return {
+        'system': 'Tangent',
+        'query': query,
+        'results': [r.latex for r in results]
+    }
         
 
 def print_help_and_exit():
-    exit('Usage: python search.py config_object query')
+    exit('Usage: python search.py config_object query [query2, ...]')
 
 if __name__ == '__main__':
 
-    if len(argv) > 2:
+    if len(argv) > 1:
         if argv[1] == 'help':
             print_help_and_exit()
+        if len(argv) == 2:
+            config = import_string(argv[1])
+            results = [search(config, query) for query in stdin.readlines()]
+            print(json.dumps(results))
         else:
             config = import_string(argv[1])
-            query = argv[2]
-            search(config, query)
+            results = [search(config, query) for query in argv[2:]]
+            print(json.dumps(results))
     else:
         print_help_and_exit()
