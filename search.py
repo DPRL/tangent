9a5df81eedbcfe03a5325993c8ccaafdc726afe3
nidhin pattaniyil <npatta01@gmail.com>
2014-03-05 14:50:35 -0500

    Added pyhton license file to top of files

--- search.py
+++ search.py
@@ -1,3 +1,28 @@
+"""
+    DPRL Math Symbol Recognizers
+    Copyright (c) 2012-2014 David Stalnaker, Richard Zanibbi
+
+    This file is part of Tangent.
+
+    Tanget is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    Tangent is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with Tangent.  If not, see <http://www.gnu.org/licenses/>.
+
+    Contact:
+        - David Stalnaker: david.stalnaker@gmail.com
+        - Richard Zanibbi: rlaz@cs.rit.edu
+"""
+
+
 from sys import argv, exit, stdin
 import json
 
@@ -17,6 +42,10 @@ def search(config, query):
 
 def print_help_and_exit():
     exit('Usage: python search.py config_object query [query2, ...]')
+
+
+
+
 
 if __name__ == '__main__':
 
--- tangent/index.py
+++ tangent/index.py
@@ -1,3 +1,27 @@
+"""
+    DPRL Math Symbol Recognizers
+    Copyright (c) 2012-2014 David Stalnaker, Richard Zanibbi
+
+    This file is part of Tangent.
+
+    Tanget is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    Tangent is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with Tangent.  If not, see <http://www.gnu.org/licenses/>.
+
+    Contact:
+        - David Stalnaker: david.stalnaker@gmail.com
+        - Richard Zanibbi: rlaz@cs.rit.edu
+"""
+
 from __future__ import division
 from collections import namedtuple
 
@@ -14,7 +38,7 @@ class Index:
     def add_directory(self, directory):
         self.add_all(SymbolTree.parse_directory(directory)[0])
 
-    def second_pass():
+    def second_pass(self):
         pass
 
 Result = namedtuple('Result', ['latex', 'score', 'debug_info', 'links', 'expr_id'])
