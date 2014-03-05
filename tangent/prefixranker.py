0530e7627dcdb77b4c5056458f6d6738a551ff31
nidhin pattaniyil <npatta01@gmail.com>
2014-03-05 14:50:35 -0500

    Added pyhton license file to top of files

--- tangent/prefixranker.py
+++ tangent/prefixranker.py
@@ -1,3 +1,27 @@
+"""
+    DPRL Math Symbol Recognizers
+    Copyright (c) 2012-2014 David Stalnaker, Richard Zanibbi
+
+    This file is part of Tangent.
+
+    Tanget is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    Tangent is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with Tangent.  If not, see <http://www.gnu.org/licenses/>.
+
+    Contact:
+        - David Stalnaker: david.stalnaker@gmail.com
+        - Richard Zanibbi: rlaz@cs.rit.edu
+"""
+
 from __future__ import division
 from collections import deque, Counter
 from itertools import izip_longest
